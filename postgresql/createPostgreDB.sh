2a4c4eb3b680f060b079e2e3b3038077706b506f
ahernandez25 <43712688+ahernandez25@users.noreply.github.com>
2020-05-31 23:35:20 -0700

    updated shell files and create.sql file

--- postgresql/createPostgreDB.sh
+++ postgresql/createPostgreDB.sh
@@ -13,17 +13,4 @@ psql -h /tmp/$LOGNAME/sockets $DBNAME < ../sql/create.sql
 
 
 
-#! /bin/bash
-# Ashlys Server
-#DBNAME=$1
-
-#echo "Checking server status"
-#pg_ctl status
-
-#echo "Copying csv files ... "
-#cp ../data/*.csv /tmp/ahern122/test/data/.
-
-#echo "Initializing tables .. "
-#sleep 1
-#psql -h /tmp/ahern122/sockets $DBNAME < ../sql/create.sql
 
--- postgresql/startPostgreSQL.sh
+++ postgresql/startPostgreSQL.sh
@@ -7,10 +7,3 @@ sleep 1
 pg_ctl -o "-c unix_socket_directories=/tmp/$LOGNAME/sockets" -D $PGDATA -l /tmp/$LOGNAME/logfile start
 
 
-#! /bin/bash
-#export PGDATA=/tmp/ahern122/test/data
-
-#sleep 1
-#Starts the database server
-#pg_ctl -o "-c unix_socket_directories=/tmp/ahern122/sockets" -D $PGDATA -l /tmp/ahern122/logfile start
-#pg_ctl -o "-c unix_socket_directories=/tmp/ahern122/sockets" -D /extra/ahern122/cs166/mydb/data -l logfile start
